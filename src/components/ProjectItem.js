65ae5051e5efefbbdaa5959b562aa9b8421c7abc
rachelsteiner91 <127536637+rachelsteiner91@users.noreply.github.com>
2023-05-01 20:22:25 -0400

    lists and keys lab complete

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+//The ProjectItem component should get a prop of technologies passed down from ProjectList. For each element in this array of technologies, create a <span> tag that displays the name of the technology.//
 function ProjectItem({ name, about, technologies }) {
   return (
     <div className="project-item">
@@ -7,6 +7,11 @@ function ProjectItem({ name, about, technologies }) {
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+        {technologies.map((technology) => {
+          return (
+            <span key={technology}>{technology}</span>
+          )
+        })}
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,23 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
+//a prop of projects is being passed down from the App component with an array of objects. For each object in the array, render one ProjectItem component with the correct props. Use the id of the project for the key prop.//
 
 function ProjectList({ projects }) {
   console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map((project) => {
+          return (
+            <ProjectItem
+              key={project.id}
+              name={project.name}
+              about={project.about}
+              technologies={project.technologies}
+            />)
+          })}
+      </div>
     </div>
   );
 }
